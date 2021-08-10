887f70ed14f6b2fb3e2b916de8d74fe7536122f4
Afrosiyob <afrosiyob.a97@gmail.com>
2021-08-10 22:38:20 +0500

    aad
    dadd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
+export class App extends Component {
 
-  const [state, setState] = useState({
-    loading: true
-  })
+  constructor ( props ) {
+    super( props );
 
-  useEffect(() => {
-    setTimeout(() => {
-      setState((prevState) => ({
-        ...prevState,
-        loading: false
-      }))
-    }, 2000);
-  })
+    this.state = {
+      counter: 0
+    }
 
-  return (
-    <div>
-      {state.loading && "loading"}
-    </div>
-  )
+
+    this.increment = () => this.setState( { counter: this.state.counter + 1 } )
+    this.decrement = () => this.setState( { counter: this.state.counter - 1 } )
+
+  }
+
+  componentDidMount () {
+    console.log( "component did mount" );
+  }
+  shouldComponentUpdate ( nextProps, nextState ) {
+    // console.log( nextProps );
+    console.log( nextState );
+    console.log( "should component updated" );
+    return true
+  }
+
+  componentDidCatch ( error, info ) {
+    console.log( error );
+    console.log( info );
+    console.log( "component did catch" );
+  }
+
+
+
+  render () {
+    return (
+      <div>
+        <button onClick={ this.increment }> increment </button>
+        <button onClick={ this.decrement } > deccrement </button>
+        Counter : { this.state.counter }
+      </div>
+    )
+  }
+
+  componentDidUpdate ( prevProps, prevState ) {
+    // console.log( prevProps );
+    console.log( prevState );
+    console.log( "component did updated" );
+  }
 }
 
 export default App

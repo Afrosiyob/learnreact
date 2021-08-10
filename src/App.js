9c5f242953e0c74d064d911e10adcaba3e40b523
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-08-10 13:08:59 +0500

    add
    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [state, setState] = useState({
+    loading: true
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState((prevState) => ({
+        ...prevState,
+        loading: false
+      }))
+    }, 2000);
+  })
+
   return (
-    <Fragment>
-      this is app
-    </Fragment>
+    <div>
+      {state.loading && "loading"}
+    </div>
   )
 }
 

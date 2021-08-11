b6ae8d3b2e1f4c447b7ef79e9396f8ae5990aba4
Afrosiyob <afrosiyob.a97@gmail.com>
2021-08-11 22:29:05 +0500

    add
    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react'
 import CounterA from './components/CounterA'
 import CounterB from './components/CounterB'
+import CounterC from './components/CounterC'
 
 const App = () => {
   return (
     <Fragment>
 
-      <CounterA />
-      <CounterB />
+      {/* <CounterA /> */ }
+      {/* <CounterB /> */ }
+      <CounterC />
     </Fragment>
   )
 }
---
+++ src/components/CounterC.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const CounterC = () => {
+    return (
+        <Fragment>
+            this is counter c
+        </Fragment>
+    )
+}
+
+export default CounterC

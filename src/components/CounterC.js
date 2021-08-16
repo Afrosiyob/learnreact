b0991b12daaf024f08e2d2ad64ddb45cb089ad9d
Afrosiyob <afrosiyob.a97@gmail.com>
2021-08-16 21:37:27 +0500

    add
    add

--- src/components/CounterC.js
+++ src/components/CounterC.js
@@ -1,9 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const CounterC = () => {
+
+
+    const [ count, setCount ] = useState( 0 )
+    const [ colored, setColored ] = useState( false )
+
+
+    const increment = () => {
+        setCount( count + 1 )
+    }
+
+    const changeColor = () => {
+        setColored( !colored )
+    }
+
     return (
         <Fragment>
-            this is counter c
+            <p style={ { color: colored ? "red" : "black" } }> Count { count } </p>
+            <button onClick={ increment }> increment count </button>
+            <button onClick={ changeColor } > change color </button>
         </Fragment>
     )
 }

6032404402b555d15154215c42d964550339f692
Afrosiyob <afrosiyob.a97@gmail.com>
2021-09-02 16:36:18 +0500

    add
    add

--- src/Test.js
+++ src/Test.js
@@ -3,19 +3,11 @@ import React from 'react'
 const Test = () => {
 
 
-    const obj1 = {
-        name: "name"
-    }
 
-    const obj2 = {
-        name: "name"
-    }
-
-    console.log( obj1 == obj2 );
 
     return (
         <div>
-            test
+            tesregerg
         </div>
     )
 }

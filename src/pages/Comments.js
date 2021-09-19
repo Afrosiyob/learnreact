ef5f1218c7d0e3933b7996cdbe31bc0d3c44e2dc
Afrosiyob <afrosiyob.a97@gmail.com>
2021-09-19 14:43:31 +0500

    add
    addd

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { useMutation } from 'react-query'
-
+import { useForm } from "react-hook-form";
 const Comments = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log( data );
 
     const { isLoading, isError, error, mutate } = useMutation( newTodo => axios.post( '/todos', newTodo ) )
     return (
         <div>
-            there are comments
+
+            <form onSubmit={ handleSubmit( onSubmit ) }>
+                {/* register your input into the hook by invoking the "register" function */ }
+                <input name="title" { ...register( "title", { required: true } ) } />
+                { errors.title && <span>This field is required</span> }
+                {/* include validation with required or other standard HTML validation rules */ }
+                <input name="body"  { ...register( "body", { required: true } ) } />
+                {/* errors will return when field validation fails  */ }
+                { errors.body && <span>This field is required</span> }
+
+                <input type="submit" />
+            </form>
         </div>
     )
 }

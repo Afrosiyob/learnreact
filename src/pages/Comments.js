1db8d19f96f4a95f9d924738a3c581a05d4a064e
Afrosiyob <afrosiyob.a97@gmail.com>
2021-09-19 14:24:18 +0500

    add
    add

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useMutation } from 'react-query'
 
 const Comments = () => {
+
+    const { isLoading, isError, error, mutate } = useMutation( newTodo => axios.post( '/todos', newTodo ) )
     return (
         <div>
             there are comments

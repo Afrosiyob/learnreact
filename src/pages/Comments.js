04431fca24a866a9e0620c84ae0b758f62e48a31
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-09-30 16:52:23 +0500

    addd
    addd

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -3,40 +3,40 @@ import { useMutation } from 'react-query'
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 const Comments = () => {
-    const { isLoading, isError, error, mutate } = useMutation( newTodo => fetch( 'https://jsonplaceholder.typicode.com/posts', {
+    const { isLoading, isError, error, mutate } = useMutation(newTodo => fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
-        body: JSON.stringify( newTodo ),
+        body: JSON.stringify(newTodo),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
-    } )
-        .then( ( response ) => response.json() )
-        .then( ( json ) => console.log( json ) ) )
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json)))
 
 
 
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log( data )
-        mutate( data )
+        console.log(data)
+        mutate(data)
     };
 
     return (
         <div>
             {
-                isLoading ? ( <div> adding new user ... </div> ) : (
-                    <form onSubmit={ handleSubmit( onSubmit ) }>
-                        {/* register your input into the hook by invoking the "register" function */ }
-                        <input name="title" { ...register( "title", { required: true } ) } />
-                        { errors.title && <span>This field is required</span> }
-                        {/* include validation with required or other standard HTML validation rules */ }
-                        <input name="body"  { ...register( "body", { required: true } ) } />
-                        {/* errors will return when field validation fails  */ }
-                        { errors.body && <span>This field is required</span> }
-                        <input name="userId" type="number" { ...register( "userId", { required: true } ) } />
-                        {/* errors will return when field validation fails  */ }
-                        { errors.userId && <span>This field is required</span> }
+                isLoading ? (<div> adding new user ... </div>) : (
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* register your input into the hook by invoking the "register" function */}
+                        <input name="title" {...register("title", { required: true })} />
+                        {errors.title && <span>This field is required</span>}
+                        {/* include validation with required or other standard HTML validation rules */}
+                        <input name="body"  {...register("body", { required: true })} />
+                        {/* errors will return when field validation fails  */}
+                        {errors.body && <span>This field is required</span>}
+                        <input name="userId" type="number" {...register("userId", { required: true })} />
+                        {/* errors will return when field validation fails  */}
+                        {errors.userId && <span>This field is required</span>}
 
                         <input type="submit" />
                     </form>
